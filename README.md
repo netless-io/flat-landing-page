44be1b693bdc6e6fbcf5b2386cd3fe205a2e89b3
hyrious <hyrious@outlook.com>
2021-04-26 09:49:43 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 <samp>-> <a href="https://flat.whiteboard.agora.io">flat.whiteboard.agora.io</a></samp>
+
+## Develop
+
+- https://vitejs.dev
+- https://github.com/vitejs/vite/tree/main/packages/plugin-vue
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>Hello, world!</p>
+    <button @click="count++">{{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<style>
+#app {
+    text-align: center;
+}
+</style>
